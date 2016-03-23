70e660c4d13769db245cc37a5b70f74ec2a077de
ghilesZ <ghiles.ziat@gmail.com>
2016-03-23 23:20:14 +0100

    is_small + is_bottom box_float

--- src/domains/box_float.ml
+++ src/domains/box_float.ml
@@ -1,6 +1,7 @@
 module Env = Map.Make (struct type t = Syntax.var let compare = compare end)
 
 module I = Itv.ItvF
+module B = I.B
 
 type t =  (Syntax.typ * I.t) Env.t
 
@@ -21,3 +22,10 @@ let is_integer (abs:t) var = fst (Env.find var abs) = Syntax.INT
 
 (* total number of variables *)
 let dimension (a:t) : int = Env.cardinal a
+
+let is_bottom (abs:t) = Env.for_all (fun k (_,v) -> I.check_bot v <> Bot.Bot) abs
+
+let is_small (abs:t) = Env.for_all (fun k (_,((b1,b2) as v)) -> 
+  not (I.is_bounded v) || 
+  B.sub_up b2 b1 > B.of_float_up !Constant.precision
+) abs
