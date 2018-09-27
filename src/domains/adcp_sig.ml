78d203c55456aef3ef5db02f66d2f5d8c73b6873
mpelleau <marie.pelleau@gmail.com>
2018-09-27 10:11:26 +0200

    put doc for abstract domains in adcp_sig.ml

--- src/domains/adcp_sig.ml
+++ src/domains/adcp_sig.ml
@@ -18,65 +18,71 @@ let not_ans = function
 module type AbstractCP = sig
 
   (*** TYPES ***)
-  (* abstract elements *)
+  (** abstract elements *)
   type t
 
-  (* returns an empty element *)
+  (** returns an empty element *)
   val empty : t
 
-  (* returns the variables *)
+  (** returns the variables annoted by their type *)
   val vars : t -> (Csp.annot * Csp.var) list
 
-  (* adds an unconstrained variable to the environnement *)
+  (** adds an unconstrained variable to the environnement *)
   val add_var : t -> Csp.annot * Csp.var -> t
 
-  (* returns the bounds of a variable *)
+  (** returns the bounds of a variable *)
   val var_bounds : t -> Csp.var -> (Mpqf.t * Mpqf.t)
 
-  (* returns the bound variables *)
+  (** returns the bound variables *)
   val bound_vars : t -> Csp.csts
 
-  (* removes an unconstrained variable from the environnement *)
+  (** removes an unconstrained variable from the environnement *)
   val rem_var : t -> Csp.var -> t
 
   (*** PREDICATES ***)
 
-  (* tests if an abstract element is too small to be cut *)
+  (** tests if an abstract element is small enough with respect to `Constant.precision` *)
   val is_small : t -> bool
 
+  (** tests if an abstract element is empty *)
   val is_empty : t -> bool
 
   (*** OPERATIONS ***)
+  (** joins two abstract elements *)
   val join: t -> t -> t
 
-  (* pruning *)
+  (** substracts the second abstract element from the first (difference operator)
+      precondition: the two abstract elements must be defined onto the same set of variables. *)
+
   val prune : t -> t -> t list * t
 
-  (* splits an abstract element *)
+  (** splits an abstract element *)
   val split : t -> t list
 
+  (** filters an abstract element with respect to an arithmetic constraint *)
   val filter : t -> (Csp.expr * Csp.cmpop * Csp.expr) -> t
 
+  (** returns the range of value of a given expression for an abstract element *)
   val forward_eval : t -> Csp.expr -> (Mpqf.t * Mpqf.t)
 
-  (* transforms an abstract element in constraints *)
+  (** transforms an abstract element into constraints *)
   val to_bexpr : t -> (Csp.expr * Csp.cmpop * Csp.expr) list
 
-  (* check if a constraint is suited for this abstract domain *)
+  (** checks if a constraint is suited for this abstract domain *)
   val is_representable : Csp.bexpr -> answer
 
-  (* printing *)
+  (** printing *)
   val print : Format.formatter -> t -> unit
 
-  (* volume *)
+  (** computes the volume of an abstract element *)
   val volume : t -> float
 
-  (* concretization function. we call it a spawner.
-     useful to do tests, and to reuse the results.
-     values are generated randomly *)
+  (** concretization function. we call it a spawner.
+      useful to do tests, and to reuse the results.
+      values are generated randomly *)
   val spawn : t -> Csp.instance
 
-  (* check if an abstract element is an abstractin of an instance *)
+  (** check if an abstract element is an abstractin of an instance *)
   val is_abstraction : t -> Csp.instance -> bool
 
 
