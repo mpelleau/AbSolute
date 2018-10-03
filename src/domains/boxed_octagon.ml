cf1fd1bf0869baced6c175b6c71c8a9b384bf0ff
Pierre Talbot <ptalbot@hyc.io>
2018-10-03 08:55:02 +0200

    [abstract domain][octagon] implement the modified version of Floyd Warshall along with the box representation of the DBM (yet untested).

--- src/domains/boxed_octagon.ml
+++ src/domains/boxed_octagon.ml
@@ -8,6 +8,7 @@
 
 open Adcp_sig
 open Csp
+open Abstract_box
 
 (* Documentation taken from the APRON project.
 
@@ -47,15 +48,20 @@ module BoxedOctagon = struct
   module F = Bound_float
   type bound = F.t
   type m = bound array
+  module I = Trigo.Make(Itv.ItvF)
+  module B = Box(I)
 
   module Env = Tools.VarMap
-  (* maps each variable to its index `i` in the matrix such that m[i,i] is the constraint vi - vi <= c *)
+  (* maps each variable to its index `i` in the matrix such that dbm[i,i] is the constraint vi - vi <= c *)
   type e = int Env.t
   type t = {
     env : e;
     (* Number of variables *)
     dim: int;
-    m : bound array
+    (* difference bound matrix *)
+    dbm : bound array;
+    (* Interval representation of the octagon (just the base variables). *)
+    box : B.t
   }
 
   let matsize : int -> int
@@ -74,20 +80,20 @@ module BoxedOctagon = struct
   let lb_idx : int -> int = fun dim -> matpos2 (dim*2) (dim*2+1)
 
   let ub : t -> int -> bound = fun o dim ->
-    F.div_up o.m.(ub_idx dim) 2.
+    F.div_up o.dbm.(ub_idx dim) 2.
 
   let lb : t -> int -> bound = fun o dim ->
-    F.div_down o.m.(lb_idx dim) (-2.)
+    F.div_down o.dbm.(lb_idx dim) (-2.)
 
   (* returns an empty element *)
-  let empty : t = { env=Env.empty; dim=0; m=[||] }
+  let empty : t = { env=Env.empty; dim=0; dbm=[||]; box=B.empty }
 
   let copy : t -> t = fun o ->
-    { env = o.env; dim=o.dim; m=Array.copy o.m }
+    { env = o.env; dim=o.dim; dbm=Array.copy o.dbm; box=o.box }
 
   (* returns the variables *)
   let vars : t -> (Csp.annot * Csp.var) list = fun o ->
-    Env.fold (fun v _ acc -> (Real, v)::acc) o.env []
+    Env.fold (fun k _ acc -> (Real, k)::acc) o.env []
 
   let check_type : Csp.annot -> unit = fun typ ->
     match typ with
@@ -105,13 +111,17 @@ module BoxedOctagon = struct
     (* allocate in the matrix two rows of size 2*dim' with a top bound. *)
     let top = F.inf in
     let row = Array.make (dim'*2*2) top in
-    let m' = Array.append o.m row in
-    { env = env'; dim = dim'; m = m' }
+    let dbm' = Array.append o.dbm row in
+    let box' = B.add_var o.box (typ, var) in
+    { env=env'; dim=dim'; dbm=dbm'; box=box' }
+
+  let var_bounds_from_dim : t -> int -> (Mpqf.t * Mpqf.t) = fun o dim ->
+    (F.to_rat (lb o dim), F.to_rat (ub o dim))
 
   (* returns the bounds of a variable *)
   let var_bounds : t -> Csp.var -> (Mpqf.t * Mpqf.t) = fun o var ->
     let dim = Env.find var o.env in
-    (F.to_rat (lb o dim), F.to_rat (ub o dim))
+    var_bounds_from_dim o dim
 
   (* returns the bound variables *)
   let bound_vars : t -> Csp.csts
@@ -130,8 +140,11 @@ module BoxedOctagon = struct
   let is_small : t -> bool
     = fun o -> volume o <= !Constant.precision
 
+  let is_failed : t -> bool
+    = fun o -> Array.exists (fun x -> x < F.zero) o.dbm
+
   let is_empty : t -> bool
-    = fun this -> Env.is_empty this.env
+    = fun o -> Env.is_empty o.env || is_failed o
 
   let check_same_env : t -> t -> unit = fun large small ->
     let checker = fun k v ->
@@ -141,22 +154,70 @@ module BoxedOctagon = struct
     try
       Env.iter checker small.env
     with Not_found ->
-      Pervasives.failwith "check_same_env: try to join/meet on two octagons s.t.\
-                           their environments is included in the other."
+      Pervasives.failwith "check_same_env: try to join two octagons s.t.\
+                           their environments is not included in the other."
 
   (*** OPERATIONS ***)
   let join : t -> t -> t = fun o o' ->
     let (res, rest) =
-      if Array.length o.m >= Array.length o'.m then
+      if Array.length o.dbm >= Array.length o'.dbm then
         (o, o')
       else
         (o', o)
     in
     let res = copy res in
+    let res = { res with box=(B.join res.box rest.box) } in
     check_same_env res rest;
-    let merge = fun idx x -> res.m.(idx) <- (F.min x res.m.(idx)) in
-    Array.iteri merge rest.m;
+    let merge = fun idx x -> res.dbm.(idx) <- (F.max x res.dbm.(idx)) in
+    Array.iteri merge rest.dbm;
     res
+
+  let meet_dbm_into_box : t -> t = fun o ->
+    let filter_box = fun k dim box ->
+      let (l,u) = var_bounds_from_dim o dim in
+      let box = B.filter box (Var k, GEQ, Cst (l, Real)) in
+      B.filter box (Var k, LEQ, Cst (u, Real)) in
+    { o with box=Env.fold filter_box o.env o.box }
+
+  let bound_add : bound -> bound -> bound = fun x y ->
+    if x = F.inf || y = F.inf then
+      F.inf
+    else
+      F.add_up x y
+
+  (* Floyd-Warshall algorithm: it propagates the octogonal constraints *)
+  let floyd_warshall_for_octagon : t -> t = fun o ->
+    let o = copy o in
+    let m = fun i j -> o.dbm.(matpos2 (i-1) (j-1)) in
+    let n = o.dim in
+    for k = 1 to n do
+      for i = 1 to (2*n) do
+        for j = 1 to (2*n) do
+          let v = List.fold_left F.min F.inf [
+            bound_add (m i (2*k)) (m (2*k) j) ;
+            bound_add (m i (2*k-1)) (m (2*k-1) j) ;
+            bound_add (bound_add (m i (2*k-1)) (m (2*k -1) (2*k))) (m (2*k) j) ;
+            bound_add (bound_add (m i (2*k)) (m (2*k) (2*k-1))) (m (2*k-1) j) ] in
+          if v < m i j then
+            o.dbm.(matpos2 (i-1) (j-1)) <- v
+        done
+      done;
+      for i = 1 to (2*n) do
+        for j = 1 to (2*n) do
+          let i' = if i mod 2 = 0 then i - 1 else i + 1 in
+          let j' = if j mod 2 = 0 then j - 1 else j + 1 in
+          if (bound_add (m i i')  (m j' j)) < (m i j) then
+            o.dbm.(matpos (i-1) (j-1)) <- (bound_add (m i i') (m j' j))
+        done
+      done
+    done;
+    for i = 1 to (2*n) do
+      if m i i < 0. then
+        raise Bot.Bot_found
+      else
+        o.dbm.(matpos2 (i-1) (i-1)) <- 0.
+    done;
+    meet_dbm_into_box o
 
   (* pruning *)
   let prune : t -> t -> t list * t
@@ -166,6 +227,7 @@ module BoxedOctagon = struct
   let split : t -> t list
     = fun o -> []
 
+  (* Throw Bot.Bot_found if an inconsistent abstract element is reached. *)
   let filter : t -> (Csp.expr * Csp.cmpop * Csp.expr) -> t
     = fun o cons -> o
 
