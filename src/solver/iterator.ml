03970f08b740143dccc5604bcd4fe73805be056c
ghilesZ <ghiles.ziat@gmail.com>
2020-09-29 10:29:21 +0200

    add iterator.ml

---
+++ src/solver/iterator.ml
@@ -0,0 +1,47 @@
+open Signature
+open Consistency
+
+(** this wrapper internalizes the constraints into an internal state
+   and then handles the order of propagations *)
+module Make (Num:AbstractCP) = struct
+
+  type space = Num.t
+
+  include Boolean.Make(Num)
+
+  type t = {
+      space: space;
+      constr: Csp.bexpr list;
+    }
+
+  let init (p:Csp.prog) : t =
+    Format.printf "variable declaration ...%!";
+    let space = List.fold_left Num.add_var Num.empty p.Csp.init in
+    Format.printf " done.\n";
+    Format.printf "constraint conversion ...%!";
+    Format.printf " done.\n%!";
+    {space; constr=p.Csp.constraints}
+
+  (* filtering constraints in turn only once *)
+  let propagate {space; constr} : t Consistency.t =
+    let rec loop sat acc abs env =
+      function
+      | [] -> Filtered ({space=abs; constr=acc},sat)
+      | c::tl ->
+         (match filter abs c with
+          | Sat -> loop sat acc abs env tl
+          | Unsat -> Unsat
+          | Filtered (abs',true) -> loop sat acc abs' env tl
+          | Filtered (abs',false) -> loop false (c::acc) abs' env tl)
+    in
+    loop true [] space Tools.VarMap.empty constr
+
+  let split elm =
+    List.rev_map (fun e -> {elm with space = e}) (Num.split elm.space [])
+
+  let spawn elm = Num.spawn elm.space
+
+  let to_result ~inner res elm =
+    if inner then Result.add_inner res elm.space
+    else Result.add_outer res elm.space
+end
