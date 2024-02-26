1f4821d1a7a9dd39c0e016c47091b1395c49fb90
ghilesZ <ghiles.ziat@gmail.com>
2024-02-26 20:04:03 +0100

    minor typo

--- absolute-solver/memabsolute.ml
+++ absolute-solver/memabsolute.ml
@@ -1,7 +1,7 @@
 (* this executable is just a wrapper that initializes a memory trace and then
-   calls absolute. This permits the monitoring of the memory usage without
-   adding Memtrace as a dependency of the solver. It is also compiled with
-   'landmarks' as a preprocessor to monitor the time consumption *)
+   calls absolute. This makes possible the monitoring of the memory usage
+   without adding Memtrace as a dependency of the solver. It is also compiled
+   with 'landmarks' as a preprocessor to monitor the time consumption *)
 
 let () =
   Memtrace.trace_if_requested ~context:"absolute" () ;
